1cf20258feeb5f4786c706fd79ccdaf5247ff64a
Hamersly <maksimwanderer96@gmail.com>
2021-09-30 20:16:36 +0500

    add Burger.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Header from './Header'
 import Order from './Order'
 import MenuAdmin from './MenuAdmin'
+import samleBurgers from '../sample-burgers'
+import Burger from './Burger'
 
 class App extends React.Component{
 	state = {
@@ -16,16 +18,40 @@ class App extends React.Component{
 		burgers[`burger${Date.now()}`] = burger;
 		// #3 Записать новый объект burgers в state
 		this.setState({burgers});
-	}
+	};
+
+	loadSamleBurgers = () => {
+		this.setState({burgers: samleBurgers});
+	};
+
+	addToOrder = (key) => {
+		// #1.Делаем копию объекта state
+		const order = {...this.state.order};
+		// #2 Добавить ключ к заказу со значением 1, либо обновить текущее значение
+		order[key] = order[key] + 1 || 1;
+		// #3.3аписать новый объект order в state
+		this.setState({order});
+	};
 
 	render(){
 		return(
 			<div className='burger-paradise'>
 				<div className='menu'>
 					<Header title="Very hot burger" amount={10} hot={true}/>
+					<ul className='burgers'>
+						{Object.keys(this.state.burgers).map(key => {
+							return <Burger 
+								key={key}
+								index={key}
+								addToOrder={this.addToOrder}
+								details={this.state.burgers[key]}/>
+						})}
+					</ul>
 				</div>
 				<Order />
-				<MenuAdmin addBurger={this.addBurger}/>
+				<MenuAdmin 
+					addBurger={this.addBurger} 
+					loadSamleBurgers={this.loadSamleBurgers}/>
 			</div>
 		)
 	};
